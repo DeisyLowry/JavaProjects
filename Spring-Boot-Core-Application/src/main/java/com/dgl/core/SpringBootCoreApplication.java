6a648fc9b045743e1d0c94b74c4570d41bb65d26
David Lowry <DavidLowry64@gmail.com>
2023-07-29 16:01:41 -0400

    Initial Commit for Core

---
+++ Spring-Boot-Core-Application/src/main/java/com/dgl/core/SpringBootCoreApplication.java
@@ -0,0 +1,14 @@
+package com.dgl.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCoreApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
